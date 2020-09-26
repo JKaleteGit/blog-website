67c30e5d2870a86cc868c4b4dee3ab670704447f
JKalete <josboit1999@gmail.com>
2020-09-26 13:39:46 +0100

    init

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Josiah Mulabi-Kalete\PycharmProjects\DjangoWeb\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
